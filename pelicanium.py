a4b1f52005474915e325f9ce259e0fb6e3f30b25
Dmitrijs Milajevs <dimazest@gmail.com>
2014-03-16 14:18:50 +0000

    Initial

---
+++ pelicanium.py
@@ -0,0 +1,4 @@
+import os.path
+
+
+theme_path = os.path.dirname(__file__)
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pelicanium',
+    version=0.1,
+    url='https://github.com/dimazest/pelicanium',
+    author="Dmitrijs Milajevs",
+    author_email="dimazest@gmail.com",
+    description='A port of the Ghostium theme to Pelican.',
+    long_description=open("README.rst").read(),
+    license='MIT',
+    packages=find_packages(),
+    package_data={'': [
+        'LICENSE',
+        'README.rst',
+        'static/*',
+        'templates/*',
+    ]
+    },
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'pelican',
+        'pelican_extended_authors'
+    ],
+)
